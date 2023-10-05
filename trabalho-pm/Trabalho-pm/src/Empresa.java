ae246eb1cc8eb5e06cbf502102a4b073e5e6e0d0
ALfLuisV <133540942+ALfLuisV@users.noreply.github.com>
2023-10-05 10:32:08 -0300

    commit claudio raio

--- trabalho-pm/Trabalho-pm/src/Empresa.java
+++ trabalho-pm/Trabalho-pm/src/Empresa.java
@@ -1,3 +1,36 @@
-public class Empresa {
-    
+public class Empresa extends Instituicao {
+    String ramo;
+    int numProfissionais;
+
+    public Empresa(String cnpj, String nome, String email, String ramo, int prof) {
+        super(cnpj, nome, email);
+        this.ramo = ramo;
+        this.numProfissionais = prof;
+        // TODO Auto-generated constructor stub
+    }
+
+
+    // GETTERS/SETTERS
+    public void setRamo(String ramo) {
+        this.ramo = ramo;
+    }
+
+    public String getRamo() {
+        return this.ramo;
+    }
+
+    public void setProfissionais(int prof) {
+        this.numProfissionais = prof;
+    }
+
+    public int getProfissionais() {
+        return this.numProfissionais;
+    }
+
+
+
+
+
+
+
 }
